1ab5f89ffcab2371ef7e5225b186e4ab864b0ed8
sylhare <sylhare@outlook.com>
2021-08-05 14:16:05 -0400

    Add more usecase

--- src/main/kotlin/RandomK.kt
+++ src/main/kotlin/RandomK.kt
@@ -7,3 +7,21 @@ inline fun <reified T> makeRandomInstanceNoArgs(): T {
 inline fun <reified T> makeRandomInstanceJVM(): T {
     return T::class.java.getDeclaredConstructor().newInstance()
 }
+
+inline fun <reified T> makeRandomInstance(): T {
+    return makeRandomInstance(T::class) as T
+}
+
+class NoUsableConstructor: Error()
+
+fun makeRandomInstance(clazz: KClass<*>): Any {
+    val constructor = clazz.constructors
+        .minByOrNull { it.parameters.size } ?: throw NoUsableConstructor()
+
+    val arguments = constructor.parameters
+        .map { it.type.classifier as KClass<*> }
+        .map { makeRandomInstance(it) }
+        .toTypedArray()
+
+    return constructor.call(*arguments)
+}
--- src/test/kotlin/RandomKTest.kt
+++ src/test/kotlin/RandomKTest.kt
@@ -6,6 +6,18 @@ import kotlin.reflect.full.IllegalCallableAccessException
 class RandomKTest {
 
     class A
+    class B(val a: A)
+    class C(val a: A, val b: B)
+    class D {
+        constructor() {
+            throw Error("Do not use this one")
+        }
+
+        constructor(a: A)
+    }
+    class P {
+        private constructor()
+    }
 
     @Test
     fun `Creates single instance using an empty constructor JVM only`() {
@@ -20,6 +32,36 @@ class RandomKTest {
         assertTrue(a is A)
         assertTrue("A@" in a.toString(), "toString of A should contains \$A@ and it is $a")
     }
+
+    @Test
+    fun `Creates a single instance using empty constructor`() {
+        val a: A = makeRandomInstance()
+        assertTrue(a is A)
+        assertTrue("A@" in a.toString(), "toString of A should contains \$A@ and it is $a")
+    }
+
+
+    @Test
+    fun `Creates a single instance using constructor with 1 variable`() {
+        val b: B = makeRandomInstance()
+        assertTrue(b is B)
+        assertTrue("\$B@" in b.toString(), "toString of A should contains \$B@ and it is $b")
+    }
+
+    @Test
+    fun `Creates a single instance using constructor with 2 variables`() {
+        val c: C = makeRandomInstance()
+        assertTrue(c is C)
+        assertTrue("\$C@" in c.toString(), "toString of A should contains \$C@ and it is $c")
+    }
+
+    @Test
+    fun `Skips constructors that cannot be used`() {
+        val d: D = makeRandomInstance()
+        assertTrue(d is D)
+        assertTrue("\$D@" in d.toString(), "toString of A should contains \$D@ and it is $d")
+    }
+
     @Test
     fun `Throws error if there is no constructor that could be used`() {
         assertThrows<IllegalCallableAccessException> { makeRandomInstance<P>() }
