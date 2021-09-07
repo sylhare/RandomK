d87e2af55f49c1d16ed6df9b35687ec80454e5af
sylhare <sylhare@outlook.com>
2021-09-07 10:11:51 -0400

    Publish on tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.wrapper.Wrapper
+
 plugins {
     kotlin("jvm") version "1.5.0"
     `maven-publish`
@@ -23,14 +25,19 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.withType<Wrapper> {
+    gradleVersion = "7.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
+
 publishing {
     repositories {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/sylhare/RandomK")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GPR_USER")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
         }
     }
@@ -40,4 +47,3 @@ publishing {
         }
     }
 }
-
