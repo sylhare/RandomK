5e76fd5a7ad7888494ff3af86ab68631585627be
sylhare <sylhare@outlook.com>
2021-10-08 08:38:56 -0400

    Update signature

--- src/main/kotlin/com/github/sylhare/RandomK.kt
+++ src/main/kotlin/com/github/sylhare/RandomK.kt
@@ -3,16 +3,12 @@ package com.github.sylhare
 import com.github.sylhare.lib.RandomBuilder
 import com.github.sylhare.lib.RandomKNotSupportedType
 import kotlin.random.Random
-import kotlin.reflect.cast
 import kotlin.reflect.typeOf
 
 
 @Throws(RandomKNotSupportedType::class)
 @ExperimentalStdlibApi
-inline fun <reified T : Any> randomK(
-    random: Random = Random,
-    config: RandomK.Config = RandomK.Config()
-): T {
+inline fun <reified T> randomK(random: Random = Random, config: RandomK.Config = RandomK.Config()): T {
     val instance = RandomBuilder(random, config).build(T::class, typeOf<T>())
     return try {
         instance as T
